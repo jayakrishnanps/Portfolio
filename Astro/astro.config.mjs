200cf8578b8c7ba835471c051286d61ccfe653a8
Jayakrishnan PS <jayakrishnanps12@gmail.com>
2026-05-30 13:59:39 +0530

    Setup GitHub Pages deployment with official Astro Action

--- Astro/astro.config.mjs
+++ Astro/astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://jayakrishnanps.github.io',
+  base: '/portfolio',
   integrations: [react()],
   output: 'static',
   vite: {
